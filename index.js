d3df28160517d2bde91b8328464b2fc2e69842ff
khalidbinibrahim <khalidbinibrahim01@gmail.com>
2024-05-10 15:56:27 +0600

    added firebase deploy links

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: [
+        'http://localhost:5173',
+        'https://khalid-bin-ibrahim-11.firebaseapp.com',
+        'https://khalid-bin-ibrahim-11.web.app'
+    ]
 }));
 app.use(express.json());
 
